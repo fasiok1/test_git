861bc349bcdf6fcd003cd9020d73394901fe183c
fasiok1 <a6yss6@gmail.com>
2023-11-06 07:18:34 +0300

    ferst
    ferst

---
+++ config.py
@@ -0,0 +1,10 @@
+user_token = 'vk1.a.8KZUfIEF-wp69L-kn-SNinVQlC7XD7QwUrd3jFuSfRUJjS0oxCK079cJRsX5KZnQxjgJ5d8W62jd40FdlYJlrNQmMajWB9LK2Ve-Oh3NUOgxo-RQvcueVvSsV-LHc7sniHl7d_MzO8cMW3JwpO1LL-uj4dLdglhAjtZi2iPs_fLsOJz4AlHxl1GngeL_XYZq7zPAT70gyEtZF0dqESRNOQ' # токен пользователя
+comm_token = '' # токен сообщества
+
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = '0000'
+db_name = 'bot_users'
--- main.py
+++ main.py
@@ -1,75 +1,34 @@
-import random
+def determinant(matrix):
 
-import pygame as pg
+    i = len(matrix)
+    j = len(matrix[0])
 
-WSIZE = (720, 480)
+    if i != j: return 'Введите матрицу квадратного вида'
+    
+    elif i == 1 and j == 1: return matrix[0][0]
 
-screen = pg.display.set_mode(WSIZE)
-
-TSIDE = 30
-MSIZE = WSIZE[0] // TSIDE, WSIZE[1] // TSIDE
-
-start_pos = MSIZE[0] // 2, MSIZE[1] // 2
-snake = [start_pos]
-alive = True
-
-direction = 0
-directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
-apple = random.randint(0, MSIZE[0]-1), random.randint(0, MSIZE[1]-1)
-
-fps = 5
-clock = pg.time.Clock()
-
-pg.font.init()
-font_score = pg.font.SysFont("Arial", 25)
-font_gameover = pg.font.SysFont("Arial", 45)
-font_space = pg.font.SysFont("Arial", 18)
-
-running = True
-while running:
-    clock.tick(fps)
-    screen.fill("black")
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-        if event.type == pg.KEYDOWN:
-            if alive:
-                if event.key == pg.K_RIGHT and direction != 2:
-                    direction = 0
-                if event.key == pg.K_DOWN and direction != 3:
-                    direction = 1
-                if event.key == pg.K_LEFT and direction != 0:
-                    direction = 2
-                if event.key == pg.K_UP and direction != 1:
-                    direction = 3
-            else:
-                if event.key == pg.K_SPACE:
-                    alive = True
-                    snake = [start_pos]
-                    apple = random.randint(0, MSIZE[0]-1), random.randint(0, MSIZE[1]-1)
-                    fps = 5
-
-    [pg.draw.rect(screen, "green", (x * TSIDE, y * TSIDE, TSIDE - 1, TSIDE - 1)) for x, y in snake]
-    pg.draw.rect(screen, "red", (apple[0] * TSIDE, apple[1] * TSIDE, TSIDE - 1, TSIDE - 1))
-
-    if alive:
-        new_pos = snake[0][0] + directions[direction][0], snake[0][1] + directions[direction][1]
-        if not (0 <= new_pos[0] < MSIZE[0] and 0 <= new_pos[1] < MSIZE[1]) or \
-                new_pos in snake:
-            alive = False
-        else:
-            snake.insert(0, new_pos)
-            if new_pos == apple:
-                fps += 1
-                apple = random.randint(0, MSIZE[0]-1), random.randint(0, MSIZE[1]-1)
-            else:
-                snake.pop(-1)
+    elif i == 2 and j == 2 : 
+        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
     else:
-        text = font_gameover.render(f"GAME OVER", True, "white")
-        screen.blit(text, (WSIZE[0] // 2 - text.get_width()//2, WSIZE[1] // 2 - 50))
-        text = font_space.render(f"Press SPACE for restart", True, "white")
-        screen.blit(text, (WSIZE[0] // 2 - text.get_width() // 2, WSIZE[1] // 2 + 10))
-    screen.blit(font_score.render(f"Score: {len(snake)}", True, "yellow"), (5, 5))
+        su_m = 0
+        for i_1 in range(i):
+            minor = []
+            for x in range(i):
+                ad = []
+                for y in range(i):
+                    if x != 0 and y != i_1:
+                        ad += [matrix[x][y]]
+                if ad != []:
+                    minor += [ad]
+            su_m += matrix[0][i_1]*determinant(minor)*(-1)**i_1
+        return su_m
 
-    pg.display.flip()
+matrix = [
+    [1,2,5,8,555],
+    [3,4,6,1,232],
+    [7,8,9,6,88],
+    [3,-3,6,4,7],
+    [3,5,2,5,42]
+]
+
+print(determinant(matrix))
